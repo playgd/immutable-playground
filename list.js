86ea5e957fe0b0cae881c6604c10c01e0e08fa5c
Fernando Daciuk <f.daciuk@gmail.com>
2016-12-27 10:30:08 -0200

    Update list

--- list.js
+++ list.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { List } = require('immutable')
+const { List, Map } = require('immutable')
 
 const list1 = List([1, 2, 3])
 console.log(list1)
@@ -9,3 +9,6 @@ const list2 = List.of(1, 2)
 console.log(list2)
 console.log(list2.size)
 console.log(list2.push(3), list2) // unchanged
+
+const list3 = List(Map({a: 1, b: 2}), Map({a: 1, b: 2}))
+console.log(list3)
